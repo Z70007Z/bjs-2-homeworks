73f9a9d963ab33a173b3e27a0f67d387be7a3715
Z70007Z <Z-7000@yandex.ru>
2022-07-23 13:58:11 +0800

    Осталось толкьо 3 ошибки только во втором задании.

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,28 +1,37 @@
 function solveEquation(a, b, c) {
-  "use strict";
-  let arr;
+  'use strict';
+  let arr = [];
   let d;
-     d = Math.pow(b,2)-4*a*c;
+     d = (b**2)-(4*a*c);
    if (d < 0) {
-      arr = [];
-   } else if (d = 0) {
-  //   let root;
-   //   root = -b/(2*a);
-      arr.push(d);
-      arr.push(-b/(2*a));
+         arr = [];
+   } else if (d === 0) {
+         arr.push(-b / (2 * a));
    } else if (d > 0) {
-      arr.push((-b + Math.sqrt(d))/(2*a));
-      arr.push((-b - Math.sqrt(d))/(2*a));   
+         arr.push((-b + Math.sqrt(d))/(2*a));
+         arr.push((-b - Math.sqrt(d))/(2*a));   
    }
   // код для задачи №1 писать здесь
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  "use strict";
+  'use strict';
   let totalAmount;
+  let paymentMonth;  // Ежемесячная оплата
+  let S = amount - contribution; // Тело кредита
+  let dataNow = new Date(); // Дата текущая
+  let n = parseInt((date.getMonth() - dataNow.getMonth()) + ((date.getFullYear() - dataNow.getFullYear()) * 12));   // На какой срок был выдан кредит
+  let P = +(percent/12)/100;  // Процентная ставка
+ 
+      // Расчет ежемесячной оплаты
+      paymentMonth = S * (P + (P / (((1 + P)**n) - 1)));
+      // Общая сумма выплат
+      totalAmount = ((paymentMonth * n)).toFixed(2);
 
-  // код для задачи №2 писать здесь
+      console.log (`Вывод: ${+totalAmount}`);
 
-  return totalAmount;
+ // код для задачи №2 писать здесь
+   
+  return +totalAmount;
 }
