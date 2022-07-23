4b8bf9626d7184c018db95057b98b0847425d68b
Z70007Z <Z-7000@yandex.ru>
2022-07-23 20:33:05 +0800

    Решение по уроку 2 (Функции)

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,24 @@
 // Задание 1
-function getArrayParams(arr) {
-  let min, max, sum, avg;
+'use strict';
 
+function getArrayParams(arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+
+      if (arr[i] > max) {
+        max = arr[i];
+      } 
+      if (arr[i] < min) {
+        min = arr[i];
+      } 
+      sum = sum + arr[i];
+    }
+
+  avg = parseFloat((sum / arr.length).toFixed(2));
   // Ваш код
 
   return { min: min, max: max, avg: avg };
@@ -9,23 +26,44 @@ function getArrayParams(arr) {
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+      sum = sum + arr[i];
+  }
   // Ваш код
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = - Infinity;
 
-  // Ваш кода
-  // for ...
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sumArr = func(arrOfArr[i]);
+    if (func(arrOfArr[i]) > max) {
+       max = sumArr;   
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
+  let max = - Infinity;
+  let min = Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      } 
+      if (arr[i] < min) {
+        min = arr[i];
+      } 
+  }
+
   // Ваш код
+  // getArrayParams(arr).max - getArrayParams(arr).min;
+  return  max - min;
 }
