278d5054f6b74d48a1bb60e58618397543798b37
Z70007Z <Z-7000@yandex.ru>
2022-08-11 21:28:48 +0800

    new level 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,16 @@
 function compareArrays(arr1, arr2) {
   let result;
 
+  result = (arr1.length === arr2.length) && (arr1.every((item, i) => item === arr2[i]));
   // Ваш код
 
-  return result; // boolean
+  return result ; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
+  resultArr = arr.filter(item1 => item1 >= 0).filter(item2 => item2 % 3 === 0).map(item3 => item3 * 10)
   // Ваш код
 
   return resultArr; // array
