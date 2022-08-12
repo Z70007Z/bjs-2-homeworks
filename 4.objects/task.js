e57258fb721ffcf648117cbde234e8a0f555b4c5
Z70007Z <Z-7000@yandex.ru>
2022-08-13 00:35:24 +0800

    Work 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,41 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+   this.name = name;
+   this. gender = gender;
+   this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+   this.subject = subjectName;
+}
+
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...arrs) {
+
+  if(this.marks === undefined){ 
+    // добавить первую оценку 
+    this.marks = [...arrs];
+    } else {
+      // добавить вторую и последующие оценки в массив
+      this.marks.push(...arrs);
+    };
+
+}
+
+Student.prototype.getAverage  = function () { 
+    return this.marks.reduce((a, b) => a + b) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+    delete Student.subject;
+    delete Student.marks;
+    this.excluded = reason; 
 }
 
 // ваш код для остальных методов
